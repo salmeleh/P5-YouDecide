52cbb065a2064d873b9d630914eae7202e12b4dc
salmeleh <salmeleh@gmail.com>
2016-05-11 08:42:55 -0500

    created venueTableViewCell

---
+++ venueSearch/venueSearch/VenueTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  VenueTableViewCell.swift
+//  venueSearch
+//
+//  Created by Stu Almeleh on 5/11/16.
+//  Copyright © 2016 Stu Almeleh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VenuteTableViewCell: UITableViewCell {
+    
+    let skPink: UIColor = UIColor(red: 248/255, green: 0, blue: 70/255, alpha: 1)
+    
+    
+    
+    
+}
