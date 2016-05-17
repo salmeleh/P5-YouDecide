bc2c1a7532140acf12da38cd41798afb2675340e
salmeleh <salmeleh@gmail.com>
2016-05-17 15:22:05 -0500

    bug #3
    scratchContext is not cleared? needs to be?

--- venueSearch/venueSearch/CoreDataStackManager.swift
+++ venueSearch/venueSearch/CoreDataStackManager.swift
@@ -40,7 +40,7 @@ class CoreDataStackManager {
     
     lazy var applicationDocumentsDirectory: NSURL = {
         
-        print("Instantiating the applicationDocumentsDirectory property")
+        //print("Instantiating the applicationDocumentsDirectory property")
         
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
@@ -49,7 +49,7 @@ class CoreDataStackManager {
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
 
-        print("Instantiating the managedObjectModel property")
+        //print("Instantiating the managedObjectModel property")
         
         let modelURL = NSBundle.mainBundle().URLForResource("Model", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
@@ -71,12 +71,12 @@ class CoreDataStackManager {
         // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         
-        print("Instantiating the persistentStoreCoordinator property")
+        //print("Instantiating the persistentStoreCoordinator property")
         
         let coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(SQLITE_FILE_NAME)
         
-        print("sqlite path: \(url.path!)")
+        //print("sqlite path: \(url.path!)")
         
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
