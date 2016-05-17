9ce65aa88daad7f5a78a3919d662d77ea1f3db9e
salmeleh <salmeleh@gmail.com>
2016-05-17 08:32:09 -0500

    lat & lng changed to NSNumber

--- venueSearch/venueSearch/Venue.swift
+++ venueSearch/venueSearch/Venue.swift
@@ -16,8 +16,8 @@ class Venue : NSManagedObject {
     //MARK: Properties
     @NSManaged var displayName: String
     @NSManaged var id: Int
-    @NSManaged var lat: Double
-    @NSManaged var lng: Double
+    @NSManaged var lat: NSNumber?
+    @NSManaged var lng: NSNumber?
     @NSManaged var street: String
     
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
@@ -35,8 +35,8 @@ class Venue : NSManagedObject {
         //Dictionary
         displayName = dictionary[SongKickClient.JSONResponseKeys.DisplayName] as! String
         id = dictionary[SongKickClient.JSONResponseKeys.ID] as! Int
-        lat = dictionary[SongKickClient.JSONResponseKeys.Lat] as! Double
-        lng = dictionary[SongKickClient.JSONResponseKeys.Lng] as! Double
+        lat = dictionary[SongKickClient.JSONResponseKeys.Lat] as? Double
+        lng = dictionary[SongKickClient.JSONResponseKeys.Lng] as? Double
         street = dictionary[SongKickClient.JSONResponseKeys.Street] as! String
     }
     
